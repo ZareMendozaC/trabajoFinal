7447865a6bc442a12c9bc4fcf005af15449dfdb3
ZareMendozaC <zare.mendozac@gmail.com>
2017-03-28 13:15:52 -0500

    maquetando los inputs de origen y destino, asi como el button de buscar

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,66 @@
-$(document).ready(function(){
-	// tu codigo va acá
-});
+var miMapa;
+var directionsService = null;
+var latLongPazPeru={
+    lat: -16.457389199999998,
+    lng: -71.5315308
+}
+var opcionesMapa = {
+    enableHighAccuracy: true
+}
+
+var destino = null;
+
+function initMap() {
+    miMapa = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: latLongPazPeru.lat,
+            lng: latLongPazPeru.lng},
+            zoom: 16
+        });
+};
+var miubicacion='no cargo la ubicacion';
+var currentMarker = null;
+var directionsDisplay = null;
+
+function geocodeLatLng(geocoder, position, id) {
+
+  var latlng = position;
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[0]) {
+        $('#'+id).html(results[0].formatted_address);
+        miubicacion= results[0].formatted_address;
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
+
+function centrarMapa(position){
+    miMapa.setZoom(16);
+    miMapa.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    currentMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: miMapa,
+        title:"Aqui estoy!!!",
+    });
+
+    var geocoder = new google.maps.Geocoder;
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    geocodeLatLng(geocoder, currentMarker.position,'direccion');
+};
+
+function init(){
+    if(navigator.geolocation){
+        console.log('Navigation supported');
+        navigator.geolocation.getCurrentPosition(centrarMapa);
+    }
+    else
+    {
+        console.log('Navigation NOT supported');
+    }
+}
