f03d64abd39f4ee1e451721094179dd57aab24ad
ZareMendozaC <zare.mendozac@gmail.com>
2017-03-28 14:30:38 -0500

    añadiendo las opciones a los combos

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,8 @@
 var miMapa;
 var directionsService = null;
-var latLongPazPeru={
-    lat: -16.457389199999998,
-    lng: -71.5315308
+var santiago={
+    lat: -33.4488897,
+    lng: -70.6692655
 }
 var opcionesMapa = {
     enableHighAccuracy: true
@@ -13,45 +13,30 @@ var destino = null;
 function initMap() {
     miMapa = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: latLongPazPeru.lat,
-            lng: latLongPazPeru.lng},
-            zoom: 16
+            lat: santiago.lat,
+            lng: santiago.lng},
+            zoom: 10
         });
 };
 var miubicacion='no cargo la ubicacion';
 var currentMarker = null;
 var directionsDisplay = null;
 
-function geocodeLatLng(geocoder, position, id) {
-
-  var latlng = position;
-  geocoder.geocode({'location': latlng}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      if (results[0]) {
-        $('#'+id).html(results[0].formatted_address);
-        miubicacion= results[0].formatted_address;
-      } else {
-        window.alert('No results found');
-      }
-    } else {
-      window.alert('Geocoder failed due to: ' + status);
-    }
-  });
-}
 
 function centrarMapa(position){
     miMapa.setZoom(16);
-    miMapa.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    miMapa.setCenter(new google.maps.LatLng( -33.4488897,-70.6692655));
     currentMarker = new google.maps.Marker({
-        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        position: new google.maps.LatLng( -33.4488897,-70.6692655),
         map: miMapa,
         title:"Aqui estoy!!!",
     });
-
+/*
     var geocoder = new google.maps.Geocoder;
     directionsService = new google.maps.DirectionsService();
     directionsDisplay = new google.maps.DirectionsRenderer();
     geocodeLatLng(geocoder, currentMarker.position,'direccion');
+    */
 };
 
 function init(){
